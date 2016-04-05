af241765b5de94f7aa6640451f90d90cef11d2dc
Daniel Schmidt <dschmidt@weluse.de>
2016-04-05 23:24:43 +0200

    added drawerLockMode example

--- DrawerLayoutExample/DrawerLayoutExample.js
+++ DrawerLayoutExample/DrawerLayoutExample.js
@@ -1,19 +1,61 @@
 'use strict';
 
 var React = require('react-native');
-var { View, Text, StyleSheet, TouchableHighlight, TextInput } = React;
+const {
+  StyleSheet,
+  Switch,
+  Text,
+  TextInput,
+  TouchableHighlight,
+  View,
+} = React;
 var DrawerLayout = require('react-native-drawer-layout');
+
+var DrawerLockModeSwitches = React.createClass({
+
+  render: function() {
+    const {
+      value,
+      onValueChange,
+    } = this.props;
+
+    return (
+      <View>
+        <View style={[styles.container, styles.split]}>
+          <Switch onValueChange={value => value ? onValueChange('unlocked') : onValueChange('unlocked')} value={value === 'unlocked'} />
+          <Text style={styles.spacedLeft}>Unlocked</Text>
+        </View>
+        <View style={[styles.container, styles.split]}>
+          <Switch onValueChange={value => value ? onValueChange('locked-closed') : onValueChange('unlocked')} value={value === 'locked-closed'} />
+          <Text style={styles.spacedLeft}>locked-closed</Text>
+        </View>
+        <View style={[styles.container, styles.split]}>
+          <Switch onValueChange={value => value ? onValueChange('locked-open') : onValueChange('unlocked')} value={value === 'locked-open'} />
+          <Text style={styles.spacedLeft}>locked-open</Text>
+        </View>
+      </View>
+    );
+  }
+});
+
 
 var DrawerLayoutExample = React.createClass({
 
   getInitialState() {
-    return {};
+    return {
+      drawerLockMode: 'unlocked',
+    };
   },
 
   render: function() {
-    var navigationView = (
+    const {
+      drawerLockMode,
+    } = this.state;
+
+    const navigationView = (
       <View style={[styles.container, {backgroundColor: '#fff'}]}>
         <Text>Hello there!</Text>
+        <DrawerLockModeSwitches value={drawerLockMode} onValueChange={value => this.setState({drawerLockMode: value})} />
         <TouchableHighlight onPress={() => this.drawer.closeDrawer()}>
           <Text>Close drawer</Text>
         </TouchableHighlight>
@@ -25,11 +67,13 @@ var DrawerLayoutExample = React.createClass({
         onDrawerSlide={(e) => this.setState({drawerSlideOutput: JSON.stringify(e.nativeEvent)})}
         onDrawerStateChanged={(e) => this.setState({drawerStateChangedOutput: JSON.stringify(e)})}
         drawerWidth={300}
+        drawerLockMode={drawerLockMode}
         ref={(drawer) => { return this.drawer = drawer  }}
         keyboardDismissMode="on-drag"
         renderNavigationView={() => navigationView}>
         <View style={styles.container}>
           <Text style={styles.welcome}>Content!</Text>
+          <DrawerLockModeSwitches value={drawerLockMode} onValueChange={value => this.setState({drawerLockMode: value})} />
           <Text>{this.state.drawerStateChangedOutput}</Text>
           <Text>{this.state.drawerSlideOutput}</Text>
           <TouchableHighlight onPress={() => this.drawer.openDrawer()}>
@@ -57,6 +101,12 @@ var styles = StyleSheet.create({
     backgroundColor: '#F2F2F2',
     height: 40,
   },
+  split: {
+    flexDirection: 'row',
+  },
+  spacedLeft: {
+    paddingLeft: 10,
+  }
 });
 
 module.exports = DrawerLayoutExample;
