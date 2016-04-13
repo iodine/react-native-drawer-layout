7da08ae1e7052927c01bce38516668bb72f99426
Daniel Schmidt <dschmidt@weluse.de>
2016-04-13 21:16:05 +0200

    fixed requirement error in a more robust way

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 const React = require('react-native');
-const { Platform } = React;
+const { Platform, DrawerLayoutAndroid } = React;
 
 if (Platform.OS === 'android') {
-  module.exports = require('./DrawerLayout.android');
+  module.exports = DrawerLayoutAndroid;
 } else if (Platform.OS === 'ios') {
   module.exports = require('./DrawerLayout.ios').default;
 }
